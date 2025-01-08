5c9fd108efb883d654c51ca48e5595e760f11ab2
colinc1991 <34336297+colinc1991@users.noreply.github.com>
2025-01-08 23:12:51 +0000

    Locked meals won't reappear when shuffling

--- src/services/meal.service.ts
+++ src/services/meal.service.ts
@@ -4,14 +4,12 @@ import { Meal } from '../dtos/meal';
 import { MealTag } from '../dtos/meal-tag';
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
 export class MealService {
     private baseMeals: Meal[] = MEAL_LIST;
     private activeMeals: Meal[] = MEAL_LIST;
-    constructor() {
-
-    }
+    constructor() { }
 
     getBaseMeals(): Meal[] {
         return [...this.baseMeals];
@@ -26,24 +24,41 @@ export class MealService {
     }
 
     // meatMeals doesn't include fish because fish is usually smoked salmon and lasts a while
-    getMeatMeals(): Meal[] {
-        return this.activeMeals.filter(x => x.tags.includes(MealTag.Beef) || x.tags.includes(MealTag.Chicken) || x.tags.includes(MealTag.Turkey));
+    getMeatMeals(): Meal[];
+    getMeatMeals(meals: Meal[]): Meal[];
+    getMeatMeals(meals?: Meal[]): Meal[] {
+        const mealsToFilter = meals ?? this.activeMeals;
+        return mealsToFilter.filter(
+            (x) =>
+                x.tags.includes(MealTag.Beef) ||
+                x.tags.includes(MealTag.Chicken) ||
+                x.tags.includes(MealTag.Turkey)
+        );
     }
 
-    getVegMeals(): Meal[] {
-        return this.activeMeals.filter(x => (x.tags.includes(MealTag.Vegetarian) || x.tags.includes(MealTag.LongLife)) && !x.tags.includes(MealTag.NonVegetarian));
+    getVegMeals(): Meal[];
+    getVegMeals(meals: Meal[]): Meal[];
+    getVegMeals(meals?: Meal[]): Meal[] {
+        const mealsToFilter = meals ?? this.activeMeals;
+        return mealsToFilter.filter(
+            (x) =>
+                (x.tags.includes(MealTag.Vegetarian) ||
+                    x.tags.includes(MealTag.LongLife)) &&
+                !x.tags.includes(MealTag.NonVegetarian)
+        );
     }
 
     shuffleMeals(array: Array<Meal>): Meal[] {
         let currentIndex = array.length;
 
         while (currentIndex != 0) {
-
             let randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex--;
 
             [array[currentIndex], array[randomIndex]] = [
-                array[randomIndex], array[currentIndex]];
+                array[randomIndex],
+                array[currentIndex],
+            ];
         }
 
         return array;
